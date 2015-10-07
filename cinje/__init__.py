8f3c755419eb853cba8deb1bb92ee168cec545ee
Alice Bevan–McGregor <alice@gothcandy.com>
2015-10-07 10:13:33 -0400

    Removed duplicated code.

--- cinje/__init__.py
+++ cinje/__init__.py
@@ -4,6 +4,6 @@ from .release import version as __version__
 # from .encoding import Translator, transform
 
 from .block import Conditional, Function, Iterator, Module, Using
-from .inline import Blank, Code, Comment, Flush, Inline, Text
+from .inline import Blank, Code, Comment, Flush, Text
 
 from .encoding import transform
--- cinje/inline/__init__.py
+++ cinje/inline/__init__.py
@@ -4,6 +4,5 @@ from .blank import Blank
 from .code import Code
 from .comment import Comment
 from .flush import Flush
-from .inline import Inline
 from .text import Text
 from .use import Use
