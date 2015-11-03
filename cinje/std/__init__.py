fe2874e3bb0bb542061d1feb6f577db2c526a532
Alice Bevan–McGregor <alice@gothcandy.com>
2015-11-03 11:41:15 -0500

    Standard HTML inline and block elements.

--- cinje/std/__init__.py
+++ cinje/std/__init__.py
@@ -1,8 +1,2 @@
 # encoding: utf-8
 
-from .blank import Blank
-from .code import Code
-from .comment import Comment
-from .flush import Flush
-from .text import Text
-from .use import Use
--- example/std.py
+++ example/std.py
@@ -1,123 +1,20 @@
-# encoding: cinje
+# encoding: utf-8
 
-## This comment won't get included in the result.
+from __future__ import print_function, unicode_literals
 
-: from collections import Mapping, Set, Sequence
+from cinje import transform
 
-: _list = list  # We override an important __builtin__ name in this module.
-
-
-: def html **kwargs
-	: """Standard HTML5 root tag."""
-<!DOCTYPE html>
-<html&{kwargs}>
-	: yield
-</html>
-: end
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import Terminal256Formatter
 
 
-: def meta name, content
-	: """HTML5 metadata."""
-	<meta name="${name}" content="${content}">
-: end
-
-
-: def page
-	: """A general HTML page."""
+def main():
+	with open('../cinje/std/html.py', 'rb') as fh:
+		lines = transform(fh.read().decode('utf8'))
 	
-	: using html
-		
-	<head>
-	</head>
-	<body>
-		
-		: yield
-	
-	</body>
-	: end
-: end
+	print(highlight(lines, PythonLexer(), Terminal256Formatter()))
 
 
-: def link href, **kwargs
-	: """HTML5 hypertext reference."""
-	<a href="${href}"&{kwargs}>
-		: yield
-	</a>
-:end
-
-
-: def div **kwargs
-	: """Generic HTML5 block element."""
-	<div&{kwargs}>
-		: yield
-	</div>
-: end
-
-
-: def span content, **kwargs
-	: """Generic HTML5 inline element."""
-	<span&{kwargs}>${content}</span>
-: end
-
-
-: def heading level=1, **kwargs
-	: """Standard level-specific HTML5 heading."""
-	<h${level}&{kwargs}>
-		: yield
-	</h${level}>
-: end
-
-
-: def abbr label, title, **kwargs
-	: """HTML5 inline abbreviation."""
-	<abbr title="${title}"&{kwargs}>${label}</abbr>
-: end
-
-
-: def list context, obj, kind='auto', flush=10, **attributes
-	: """Generate an HTML5 list, defaulting to type auto-detection."""
-	
-	: if kind == 'auto'
-		: if isinstance(obj, Mapping)
-			: kind = 'dl'
-		: elif isinstance(obj, Set)
-			: kind = 'ul'
-		: elif isinstance(obj, Sequence)
-			: kind = 'ol'
-		: end
-	: end
-	
-	<${kind}&{kwargs}>
-		: if kind == 'dl'
-			: for key in obj
-				<dt>${key}</dt>
-				<dd>${obj[key]}</dd>
-			: end
-		: else
-			: for element in obj
-				<li>${element}</li>
-			: end
-		: end
-	</${kind}>
-: end
-
-
-: def page_sample content
-	Some stuff prior to the page inclusion.
-	
-	: flush
-	
-	: using page
-		
-		:flush
-		
-		<p>This is some content:</p>
-		
-		#{content}
-		
-		:flush
-		
-	: end
-	
-	Some stuff after the page inclusion.
-: end
+if __name__ == '__main__':
+	main()
