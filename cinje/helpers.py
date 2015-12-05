b78662b105c88e61ba81e6ed9259fbe1903fa9ec
Alice Bevan–McGregor <alice@gothcandy.com>
2015-12-05 05:37:26 -0500

    Unification of version-specific code.

--- cinje/helpers.py
+++ cinje/helpers.py
@@ -1,26 +1,21 @@
 # encoding: utf-8
 # pragma: no cover
 
-try:
-	unicode = unicode
-except:
-	unicode = str
+from json import dumps as _json
+
+from .util import str, iterate, xmlargs, interruptable as _interrupt, Pipe as pipe
 
 try:
 	from markupsafe import Markup as bless, escape_silent as escape
 except ImportError:
-	bless = unicode
+	bless = str
 	try:
 		from html import escape as __escape
 	except:
 		from cgi import escape as __escape
 	
 	def escape(value):
-		return __escape(unicode(value))
-
-from json import dumps as _json
-
-from .util import iterate, xmlargs, interruptable as _interrupt, Pipe as pipe
+		return __escape(str(value))
 
 
-__all__ = ['unicode', 'bless', 'escape', 'iterate', 'xmlargs', '_interrupt', 'pipe']
+__all__ = ['bless', 'escape', 'iterate', 'xmlargs', '_interrupt', 'pipe']
