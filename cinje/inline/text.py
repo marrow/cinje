aa6f9d24a3b155139e6173cd5e92cd65d8ecd6cb
Alice Bevan–McGregor <alice@gothcandy.com>
2016-03-24 00:45:02 -0400

    Reduce mccabe complexity somewhat by factoring out isolatable token handlers.

--- cinje/inline/text.py
+++ cinje/inline/text.py
@@ -55,10 +55,47 @@ class Text(object):
 	def match(self, context, line):
 		return line.kind == 'text'
 	
+	def process_text(self, chunk_, scope, lineno, prefix, suffix, last, first):
+		chunk_ = pformat(
+				chunk_,
+				indent = 0,
+				width = 120 - 4 * scope,
+			).replace("\n ", "\n").strip().lstrip('(').rstrip(')')
+		
+		for line in iterate(chunk_.split('\n')):
+			value = line.value
+			
+			if line.first and prefix:
+				value = prefix + value
+			
+			if suffix:
+				value += suffix
+			
+			yield Line(lineno, value, scope)
+		
+		if last and not first:
+			yield Line(lineno, self.SUFFIX, scope - 1)  # End the call to _buffer.extend()
+	
+	def process_format(self, chunk_):
+		# We need to split the expression defining the format string from the values to pass when formatting.
+		# We want to allow any Python expression, so we'll need to piggyback on Python's own parser in order
+		# to exploit the currently available syntax.  Apologies, this is probably the scariest thing in here.
+		split = -1
+		
+		try:
+			ast.parse(chunk_)
+		except SyntaxError as e:  # We expect this, and catch it.  It'll have exploded after the first expr.
+			split = chunk_.rfind(' ', 0, e.offset)
+		
+		token = '_bless(' + chunk_[:split].rstrip() + ').format'
+		chunk_ = chunk_[split:].lstrip()
+		
+		return token, chunk_
+	
 	def __call__(self, context):
 		dirty = False  # Used for conditional flushing.
 		prefix = ''  # Prepend to the source line emitted.
-		suffix = ''   # Append to the source line emitted.
+		suffix = ''  # Append to the source line emitted.
 		
 		# Make sure we have a buffer to write to.
 		for i in ensure_buffer(context):
@@ -91,41 +128,13 @@ class Text(object):
 				suffix += ','
 			
 			if token == 'text':
-				chunk_ = pformat(
-						chunk_,
-						indent = 0,
-						width = 120 - 4 * scope,
-					).replace("\n ", "\n").strip()
-				
-				for line in iterate(chunk_.split('\n')):
-					value = line.value
-					
-					if line.first and prefix:
-						value = prefix + value
-					
-					if suffix:
-						value += suffix
-					
-					yield Line(lineno, value, scope)
-				
-				if last and not first:
-					yield Line(lineno, self.SUFFIX, scope - 1)  # End the call to _buffer.extend()
+				for line in self.process_text(chunk_, scope, lineno, prefix, suffix, last, first):
+					yield line
 				
 				continue
 			
 			if token == 'format':
-				# We need to split the expression defining the format string from the values to pass when formatting.
-				# We want to allow any Python expression, so we'll need to piggyback on Python's own parser in order
-				# to exploit the currently available syntax.  Apologies, this is probably the scariest thing in here.
-				split = -1
-				
-				try:
-					ast.parse(chunk_)
-				except SyntaxError as e:  # We expect this, and catch it.  It'll have exploded after the first expr.
-					split = chunk_.rfind(' ', 0, e.offset)
-				
-				token = '_bless(' + chunk_[:split].rstrip() + ').format'
-				chunk_ = chunk_[split:].lstrip()
+				token, chunk_ = self.process_format(chunk_)
 			
 			yield Line(lineno, prefix + token + '(' + chunk_ + ')' + suffix, scope)
 			
