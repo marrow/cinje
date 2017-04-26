30d5a72f5e59f8020e3e4e5308c68a0eee261cc4
Alice Bevan-McGregor <alice@gothcandy.com>
2017-04-26 03:58:12 -0400

    Galfi function transformer.

--- cinje/block/function.py
+++ cinje/block/function.py
@@ -1,153 +1,115 @@
 # encoding: utf-8
 
-import re
+from __future__ import unicode_literals
 
-from ..util import py, pypy, ensure_buffer
+from marrow.dsl.block.function import FunctionTransformer
 from ..inline.flush import flush_template
+from ..util import ensure_buffer
 
 
+log = __import__('logging').getLogger(__name__)
 
-class Function(object):
+
+class CinjeFunctionTransformer(FunctionTransformer):
 	"""Proces function declarations within templates.
+	
+	Used to track if the given function is a template function or not, transform the argument list if such optimization
+	is warranted, and to add the requisite template processing glue suffix. Functions increase scope.
 	
 	Syntax:
 	
 		: def <name> <arguments>
 		: end
 	
+	Inherits:
+	
+	* `name` - the name of the function
+	* `buffer` - the named collection of buffers
+	
+	Tracks:
+	
+	* `helpers` - helpers utilized within this template function
+	* `added` - context tags added via annotation
+	* `removed` - context tags removed via annotation
+	
+	As a reminder, functions are divided into:
+	
+	* `decorator`
+	* `declaration`
+	* `docstring`
+	* `prefix`
+	* `function`
+	* `suffix`
+	* `trailer`
 	"""
 	
-	priority = -50
+	__slots__ = ('helpers', 'added', 'removed')
 	
-	# Patterns to search for bare *, *args, or **kwargs declarations.
-	STARARGS = re.compile(r'(^|,\s*)\*([^*\s,]+|\s*,|$)')
-	STARSTARARGS = re.compile(r'(^|,\s*)\*\*\S+')
+	def __init__(self, decoder):
+		super(CinjeFunctionTransformer, self).__init__(decoder)
+		
+		self.helpers = set()  # Specific helpers utilized within the function.
+		self.added = set()  # Flags added through annotation.
+		self.removed = set()  # Flags removed through annotation.
 	
-	# Automatically add these as keyword-only scope assignments.
-	OPTIMIZE = ['_escape', '_bless', '_args']
-	
-	def match(self, context, line):
-		"""Match code lines using the "def" keyword."""
-		return line.kind == 'code' and line.partitioned[0] == 'def'
-	
-	def _optimize(self, context, argspec):
-		"""Inject speedup shortcut bindings into the argument specification for a function.
+	def process_declaration(self, context, declaration):
+		line, = declaration  # Cinje declarations can only be one line... for now.
 		
-		This assigns these labels to the local scope, avoiding a cascade through to globals(), saving time.
+		text, _, annotation = line.line.partition(' ')[2].rpartition('->')
 		
-		This also has some unfortunate side-effects for using these sentinels in argument default values!
-		"""
-		
-		argspec = argspec.strip()
-		optimization = ", ".join(i + "=" + i for i in self.OPTIMIZE)
-		split = None
-		prefix = ''
-		suffix = ''
-		
-		if argspec:
-			matches = list(self.STARARGS.finditer(argspec))
-			
-			if matches:
-				split = matches[-1].span()[1]  # Inject after, a la "*args>_<", as we're positional-only arguments.
-				if split != len(argspec):
-					prefix = ', ' if argspec[split] == ',' else ''
-					suffix = '' if argspec[split] == ',' else ', ' 
-			
-			else:  # Ok, we can do this a different way…
-				matches = list(self.STARSTARARGS.finditer(argspec))
-				prefix = ', *, '
-				suffix = ', '
-				if matches:
-					split = matches[-1].span()[0]  # Inject before, a la ">_<**kwargs".  We're positional-only arguments.
-					if split == 0:
-						prefix = '*, '
-					else:
-						suffix = ''
-				else:
-					split = len(argspec)
-					suffix = ''
-		
-		else:
-			prefix = '*, '
-		
-		if split is None:
-			return prefix + optimization + suffix
-		
-		return argspec[:split] + prefix + optimization + suffix + argspec[split:]
-	
-	def __call__(self, context):
-		input = context.input
-		
-		declaration = input.next()
-		line = declaration.partitioned[1]  # We don't care about the "def".
-		line, _, annotation = line.rpartition('->')
-		
-		if annotation and not line:  # Swap the values back.
-			line = annotation
+		if annotation and not text:  # Swap the values back.
+			text = annotation
 			annotation = ''
 		
-		name, _, line = line.partition(' ')  # Split the function name.
+		name, _, text = text.partition(' ')  # Split the function name out.
 		
-		argspec = line.rstrip()
-		name = name.strip()
+		argspec = text.rstrip()
+		name = self.name = name.strip()
 		annotation = annotation.lstrip()
-		added_flags = []
-		removed_flags = []
+		annotation = {'!dirty', '!text', '!using'} | set(i.lower().strip() for i in annotation.split())
 		
-		if annotation:
-			for flag in (i.lower().strip() for i in annotation.split()):
-				if not flag.strip('!'): continue  # Handle standalone exclamation marks.
+		# TODO: Re-introduce positional named local scoping optimization for non-Pypy runtimes.
+		# TODO: Generalize flag processing like this into galfi.
+		
+		for flag in annotation:
+			if not flag.strip('!'): continue  # Ignore standalone exclamation marks.
+			
+			if flag[0] == '!':
+				flag = flag[1:]
 				
-				if flag[0] == '!':
-					flag = flag[1:]
-					
-					if flag in context.flag:
-						context.flag.remove(flag)
-						removed_flags.append(flag)
-					
-					continue
+				if flag in context:  # We do this rather than discard to track.
+					context.remove(flag)
+					self.removed.add(flag)
 				
-				if flag not in context.flag:
-					context.flag.add(flag)
-					added_flags.append(flag)
+				continue
+			
+			if flag not in context:
+				context.add(flag)
+				self.added.add(flag)
 		
-		if py == 3 and not pypy:
-			argspec = self._optimize(context, argspec)
+		line = line.clone(line='def ' + name + '(' + argspec + '):')
 		
-		# Reconstruct the line.
+		for line in super(CinjeFunctionTransformer, self).process_declaration(context, [line]):
+			yield line
+	
+	def egress(self, context):
+		"""Code to be executed when exiting the context of a function.
 		
-		line = 'def ' + name + '(' + argspec + '):'
+		Always call super() last in any subclasses.
+		"""
 		
-		# yield declaration.clone(line='@cinje.Function.prepare')  # This lets us do some work before and after runtime.
-		yield declaration.clone(line=line)
+		if 'dirty' in context:
+			self.suffix.append(*flush_template(context, reconstruct=False))
 		
-		context.scope += 1
+		if 'text' in context:
+			self.prefix.append(*ensure_buffer(context, False))
+			context.module.templates.add(self.name)
 		
-		for i in ensure_buffer(context, False):
-			yield i
+		if 'using' in context:
+			self.prefix.append('_using_stack = []')
 		
-		for i in context.stream:
-			yield i
+		context.module.helpers.update(self.helpers)
 		
-		if 'using' in context.flag:  # Clean up that we were using things.
-			context.flag.remove('using')
-		
-		if 'text' in context.flag:
-			context.templates.append(name)
-		
-		for i in flush_template(context, reconstruct=False):  # Handle the final buffer yield if any content was generated.
-			yield i
-		
-		if 'text' in context.flag:
-			context.flag.remove('text')
-		
-		for flag in added_flags:
-			if flag in context.flag:
-				context.flag.remove(flag)
-		
-		for flag in removed_flags:
-			if flag not in context.flag:
-				context.flag.add(flag)
-		
-		context.scope -= 1
-
+		# Reset the manipulated flags to their original state.
+		context.flag.discard(self.added)
+		context.flag.update(self.removed)
